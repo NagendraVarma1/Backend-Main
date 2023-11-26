dd99f9c506266bfa1585e111b1e5dc85b02f6236
NagendraVarma1 <nagendravarmal512@gmail.com>
2023-11-26 10:10:57 +0530

    message displaying on screen

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const http = require("http");
 const fs = require("fs");
-const { parse } = require("path");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+  const body = [];
 
   if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Practice</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>Practice</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
     req.on("data", (chunk) => {
       console.log(chunk);
       body.push(chunk);
@@ -31,14 +34,15 @@ const server = http.createServer((req, res) => {
         return res.end();
       });
     });
+  } else {
+    res.setHeader("Content-type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome to my Node Page</h1></body>");
+    res.write("</html>");
+    res.end();
   }
-
-  res.setHeader("Content-type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Welcome to my Node Page</h1></body>");
-  res.write("</html>");
-  res.end();
 });
 
 server.listen(3000);
+
